d1d49e3bc9c6b6a5d7c3c67593dfeba5b73d86dc
vishwa <vishwa@gmail.com>
2026-04-18 02:40:16 +0530

    Initial Commit

--- WeatherAqiFrontend/src/main.ts
+++ WeatherAqiFrontend/src/main.ts
@@ -1,14 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-    provideHttpClient()]
-};
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
