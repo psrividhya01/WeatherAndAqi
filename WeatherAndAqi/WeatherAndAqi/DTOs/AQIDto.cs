78d6bf82244c93ab69cd12ac93aca8cd53579165
Adhi290328 <adhiseshanadhiseshan001@gmail.com>
2026-04-18 12:56:53 +0530

    Finalized module 5 and repository cleanup

--- WeatherAndAqi/WeatherAndAqi/DTOs/AQIDto.cs
+++
@@ -1,8 +0,0 @@
-namespace WeatherAndAqi.DTOs;
-
-public class AQIDto
-{
-    
-    public int AQI { get; set; }
-    public string Category { get; set; }
-}
--- WeatherAndAqi/WeatherAndAqi/Interfaces/IWeatherService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using WeatherAndAqi.DTOs;
-
-namespace WeatherAndAqi.Interfaces;
-
-public interface IWeatherService
-{
-    Task<WeatherResponseDto> GetWeatherDashboardAsync(string city);
-}
